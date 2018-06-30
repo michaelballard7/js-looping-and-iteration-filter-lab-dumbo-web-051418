b68b545585afbcdb846579812951c0f899475e4d
Michael Ballard <ballard.michael9@gmail.com>
2018-06-30 13:55:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function findMatching(collection, string) {
   return collection.filter(function(driver){
-   return driver.toLowerCase === string.toLowerCase
+   return driver.toLowerCase() === string.toLowerCase()
   })
 }
 

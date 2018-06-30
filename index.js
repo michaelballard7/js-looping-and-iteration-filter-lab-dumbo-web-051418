999a2759ad8bae42122276cebfba849cf2feae19
Michael Ballard <ballard.michael9@gmail.com>
2018-06-30 14:01:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file
+
+function findMatching(collection, string) {
+  return collection.filter(function(driver){
+   return driver.toLowerCase() === string.toLowerCase()
+  })
+}
+
+
+function fuzzyMatch(collection, string){
+  return collection.filter(function(driver){
+    return driver.split("")[0] === string.split("")[0]
+  })
+  
+}
+
+function matchName(collection, string){
+  return collection.filter(function(driver){
+    return driver.name.toLowerCase() === string.toLowerCase()
+  })
+  
+}

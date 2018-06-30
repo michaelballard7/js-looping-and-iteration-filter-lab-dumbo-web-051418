cfc0405e5e5821479c964c610c45fb847a796975
Michael Ballard <ballard.michael9@gmail.com>
2018-06-30 13:49:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 // Code your solution in this file
 
-function findMatching()
+function findMatching(collection, string) {
+  
+}
+
+
+function fuzzyMatch(collection, string){
+  
+}
+
+function matchName(collection, sting){
+  
+}
